3f4769aa6e1d265c0897cc8050b8530d841fedfd
deon-gracias <deongracias1@gmail.com>
2023-02-05 15:32:50 +0530

    Added reject unauthorized

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false,
+        rejectUnauthorized: true,
       },
     },
     operatorsAliases: false,
